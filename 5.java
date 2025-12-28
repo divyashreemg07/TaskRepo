78330d6cc33e1505a69ca5ed325e4a2c78c192d1
divyashreemg07 <divyashreemg02@gmail.com>
2025-12-29 00:10:09 +0530

    Resolve merge conflict in 5.java

--- 5.java
+++ 5.java
@@ -1,6 +1,6 @@
 class Example {
     public static void main(String[] args) {
-        System.out.print("Hello from main");
+        System.out.print("Hello from m branch")
         System.out.println("Im Nidhi from Bengaluru");
         System.out.print("end")
     }

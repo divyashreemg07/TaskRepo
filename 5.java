dadf2a4eeb8b87b441d8b62b3c9e1b868be1824c
divyashreemg07 <divyashreemg02@gmail.com>
2025-12-28 23:55:19 +0530

    5.java is changed(Hello to Hi and Bye to end

--- 5.java
+++ 5.java
@@ -1,7 +1,7 @@
 class Example {
     public static void main(String[] args) {
-        System.out.print("Hello")
+        System.out.print("Hi")
         System.out.println("Im Nidhi from Bengaluru");
-        System.out.print("Bye")
+        System.out.print("end")
     }
 }

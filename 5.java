d237ac762abaa89df9fb10b1c5988354bb134411
divyashreemg07 <divyashreemg02@gmail.com>
2025-12-28 23:59:36 +0530

    main:change Hello line

--- 5.java
+++ 5.java
@@ -1,6 +1,6 @@
 class Example {
     public static void main(String[] args) {
-        System.out.print("Hi")
+        System.out.print("Hello from main");
         System.out.println("Im Nidhi from Bengaluru");
         System.out.print("end")
     }

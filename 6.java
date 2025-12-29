6f3ca5e4828f56351df2a0e9f49f6f7a4a1eb72d
divyashreemg07 <divyashreemg02@gmail.com>
2025-12-29 12:11:20 +0530

    7.java is added

--- 6.java
+++ 6.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Welcome to Java Programming");
+        System.out.println("Hi,Welcome to VS code");
     }
 }

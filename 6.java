94b0a080901fc9d6b89a39493dce3b88d8c875b2
divyashreemg07 <divyashreemg02@gmail.com>
2025-12-29 12:15:28 +0530

    7.java is changed

--- 6.java
+++ 6.java
@@ -1,5 +1,9 @@
 class HelloWorld {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         System.out.println("Hi,Welcome to VS code");
+=======
+        System.out.println("Welcome to Python Programming");
+>>>>>>> Stashed changes
     }
 }
